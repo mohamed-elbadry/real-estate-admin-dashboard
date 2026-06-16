8a927f65001dcbde256a7f557f92b7a68a4e771a
mohamed-elbadry <mohmedelbadry25@gmail.com>
2026-06-16 12:04:02 +0300

    .side

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterLinkActive
-  ],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+
+  @Input() sidebarOpen = false;
+
+  @Output() closeSidebar = new EventEmitter<void>();
+
+  close() {
+    this.closeSidebar.emit();
+  }
+
+}
